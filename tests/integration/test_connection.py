19dddd4d9ba969ebd01951d9a4235a3623a38efa
KimYen Ladia <kimyen0816@gmail.com>
2019-06-17 16:55:21 -0700

    add travis yml file
    for travis build

--- tests/integration/test_connection.py
+++ tests/integration/test_connection.py
@@ -4,4 +4,4 @@ from spccore.connection import *
 def test_get_version():
     synapse_connection = get_connection()
     version = synapse_connection.get("/version")
-    print(version)
+    assert version['version'] is not None
