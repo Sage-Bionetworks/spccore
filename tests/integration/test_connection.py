2afb13e5b9903d83c1a5a6fd2f8ff6afab31c1cf
KimYen Ladia <kimyen0816@gmail.com>
2019-06-18 14:27:09 -0700

    add test

--- tests/integration/test_connection.py
+++ tests/integration/test_connection.py
@@ -1,7 +1,13 @@
-from spccore.connection import *
+from tests.integration.utils import *
 
 
-def test_get_version():
-    synapse_connection = get_connection()
+def test_anonymous_connection():
+    synapse_connection = get_connection(base_endpoint=get_test_endpoint())
     version = synapse_connection.get("/version")
     assert version['version'] is not None
+
+
+def test_user_connection():
+    synapse_connection = get_test_user_connection()
+    profile = synapse_connection.get("/userProfile")
+    assert profile['username'] == 'test'
