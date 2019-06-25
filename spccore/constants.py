ecad12abb5982de3519c7e0623129cddfc94d0b3
KimYen Ladia <kimyen0816@gmail.com>
2019-06-25 16:36:40 -0700

    add more tests

--- spccore/constants.py
+++ spccore/constants.py
@@ -2,6 +2,13 @@ import requests
 from .__version__ import __version__
 
 
+# Generic constants
+
+ISO_FORMAT = "%Y-%m-%dT%H:%M:%S.000Z"
+CONTENT_TYPE_HEADER = 'content-type'
+JSON_CONTENT_TYPE = 'application/json'
+
+
 # Synapse specific constants
 
 SYNAPSE_DEFAULT_REPO_ENDPOINT = "https://repo-prod.prod.sagebase.org/repo/v1"
@@ -14,13 +21,7 @@ SYNAPSE_SIGNATURE_HEADER = 'signature'
 SYNAPSE_USER_AGENT_HEADER = {'User-Agent': 'spccore/{version} {default}'.format(
     **{'version': __version__, 'default': requests.utils.default_user_agent()})}
 
-SYNAPSE_DEFAULT_HTTP_HEADERS = {'content-type': 'application/json; charset=UTF-8',
+SYNAPSE_DEFAULT_HTTP_HEADERS = {CONTENT_TYPE_HEADER: 'application/json; charset=UTF-8',
                                 'Accept': 'application/json; charset=UTF-8'}
 
 SYNAPSE_DEFAULT_STORAGE_LOCATION_ID = 1
-
-# Generic constants
-
-ISO_FORMAT = "%Y-%m-%dT%H:%M:%S.000Z"
-CONTENT_TYPE_HEADER = 'content-type'
-JSON_CONTENT_TYPE = 'application/json'
