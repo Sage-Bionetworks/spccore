40c1c97cbca9bfa6778cc435a1353bcfc3c005f3
KimYen Ladia <kimyen0816@gmail.com>
2019-06-27 17:16:27 -0700

    add unit tests

--- tests/unit/test_baseclient.py
+++ tests/unit/test_baseclient.py
@@ -123,7 +123,7 @@ def test__generate_request_url_invalid_path():
         _generate_request_url("https://synapse.org", "http:/path.com/path")
 
 
-def test__generate_request_url_invalid_endpoint():
+def test__generate_request_url():
     assert _generate_request_url("https://synapse.org", "/entity") == "https://synapse.org/entity"
 
 
@@ -149,3 +149,190 @@ def test_get_base_client():
 
 
 # SynapseBaseClient
+
+class TestSynapseBaseClient:
+
+    def test_constructor(self):
+        repo_endpoint = "repo"
+        auth_endpoint = "auth"
+        file_endpoint = "file"
+        username = "x"
+        api_key = base64.b64encode(b"I am an api key").decode()
+        client = SynapseBaseClient(repo_endpoint=repo_endpoint,
+                                   auth_endpoint=auth_endpoint,
+                                   file_endpoint=file_endpoint,
+                                   username=username,
+                                   api_key=api_key)
+        assert client._default_repo_endpoint == repo_endpoint
+        assert client._default_auth_endpoint == auth_endpoint
+        assert client._default_file_endpoint == file_endpoint
+        assert client._username == username
+        assert client._api_key == base64.b64decode(api_key)
+        assert client._requests_session is not None
+
+    @pytest.fixture
+    def client_setup(self):
+        username = 'x'
+        api_key = base64.b64encode(b"I am an api key").decode()
+        return username, api_key, SynapseBaseClient(username=username, api_key=api_key)
+
+    @pytest.fixture
+    def test_data(self):
+        req_response = Mock(requests.Response)
+        path = "/entity/syn123"
+        headers = SYNAPSE_USER_AGENT_HEADER
+        stub_response = {'id': 'syn123'}
+        params = {'key': 'value'}
+        return req_response, path, headers, stub_response, params
+
+    # get
+
+    def test_get_use_default_endpoint(self, client_setup, test_data):
+        username, api_key, client = client_setup
+        req_response, path, headers, stub_response, params = test_data
+
+        with patch.object(requests.Session, 'get', return_value=req_response) as mock_req_get, \
+                patch('spccore.baseclient._handle_response', return_value=stub_response) as mock_handle, \
+                patch('spccore.baseclient._generate_signed_headers', return_value=headers) as mock_sign_headers, \
+                patch.object(req_response, "json", return_value=json):
+            assert client.get(path, request_parameters= params) == stub_response
+            mock_req_get.assert_called_once_with(SYNAPSE_DEFAULT_REPO_ENDPOINT+path, headers=headers, params=params)
+            mock_handle.assert_called_once_with(req_response)
+            mock_sign_headers.assert_called_once_with(SYNAPSE_DEFAULT_REPO_ENDPOINT+path,
+                                                      username=username,
+                                                      api_key=base64.b64decode(api_key),
+                                                      headers=None)
+
+    def test_get_custom_endpoint(self, client_setup, test_data):
+        username, api_key, client = client_setup
+        req_response, path, headers, stub_response, params = test_data
+        endpoint = "https://repo-dev.dev.sagebase.org/repo/v1"
+
+        with patch.object(requests.Session, 'get', return_value=req_response) as mock_req_get, \
+                patch('spccore.baseclient._handle_response', return_value=stub_response) as mock_handle, \
+                patch('spccore.baseclient._generate_signed_headers', return_value=headers) as mock_sign_headers, \
+                patch.object(req_response, "json", return_value=json):
+            assert client.get(path, request_parameters= params, endpoint=endpoint) == stub_response
+            mock_req_get.assert_called_once_with(endpoint+path, headers=headers, params=params)
+            mock_handle.assert_called_once_with(req_response)
+            mock_sign_headers.assert_called_once_with(endpoint+path,
+                                                      username=username,
+                                                      api_key=base64.b64decode(api_key),
+                                                      headers=None)
+
+    # post
+
+    def test_post_use_default_endpoint(self, client_setup, test_data):
+        username, api_key, client = client_setup
+        req_response, path, headers, body, params = test_data
+
+        with patch.object(requests.Session, 'post', return_value=req_response) as mock_req_post, \
+                patch('spccore.baseclient._handle_response', return_value=body) as mock_handle, \
+                patch('spccore.baseclient._generate_signed_headers', return_value=headers) as mock_sign_headers, \
+                patch.object(req_response, "json", return_value=json):
+            assert client.post(path, body, request_parameters=params) == body
+            mock_req_post.assert_called_once_with(SYNAPSE_DEFAULT_REPO_ENDPOINT + path,
+                                                  data=json.dumps(body),
+                                                  headers=headers,
+                                                  params=params)
+            mock_handle.assert_called_once_with(req_response)
+            mock_sign_headers.assert_called_once_with(SYNAPSE_DEFAULT_REPO_ENDPOINT + path,
+                                                      username=username,
+                                                      api_key=base64.b64decode(api_key),
+                                                      headers=None)
+
+    def test_post_custom_endpoint(self, client_setup, test_data):
+        username, api_key, client = client_setup
+        req_response, path, headers, body, params = test_data
+        endpoint = "https://repo-dev.dev.sagebase.org/repo/v1"
+
+        with patch.object(requests.Session, 'post', return_value=req_response) as mock_req_post, \
+                patch('spccore.baseclient._handle_response', return_value=body) as mock_handle, \
+                patch('spccore.baseclient._generate_signed_headers', return_value=headers) as mock_sign_headers, \
+                patch.object(req_response, "json", return_value=json):
+            assert client.post(path, body, request_parameters=params, endpoint=endpoint) == body
+            mock_req_post.assert_called_once_with(endpoint + path,
+                                                  data=json.dumps(body),
+                                                  headers=headers,
+                                                  params=params)
+            mock_handle.assert_called_once_with(req_response)
+            mock_sign_headers.assert_called_once_with(endpoint + path,
+                                                      username=username,
+                                                      api_key=base64.b64decode(api_key),
+                                                      headers=None)
+
+    # put
+
+    def test_put_use_default_endpoint(self, client_setup, test_data):
+        username, api_key, client = client_setup
+        req_response, path, headers, body, params = test_data
+
+        with patch.object(requests.Session, 'put', return_value=req_response) as mock_req_put, \
+                patch('spccore.baseclient._handle_response', return_value=body) as mock_handle, \
+                patch('spccore.baseclient._generate_signed_headers', return_value=headers) as mock_sign_headers, \
+                patch.object(req_response, "json", return_value=json):
+            assert client.put(path, body, request_parameters=params) == body
+            mock_req_put.assert_called_once_with(SYNAPSE_DEFAULT_REPO_ENDPOINT + path,
+                                                  data=json.dumps(body),
+                                                  headers=headers,
+                                                  params=params)
+            mock_handle.assert_called_once_with(req_response)
+            mock_sign_headers.assert_called_once_with(SYNAPSE_DEFAULT_REPO_ENDPOINT + path,
+                                                      username=username,
+                                                      api_key=base64.b64decode(api_key),
+                                                      headers=None)
+
+    def test_put_custom_endpoint(self, client_setup, test_data):
+        username, api_key, client = client_setup
+        req_response, path, headers, body, params = test_data
+        endpoint = "https://repo-dev.dev.sagebase.org/repo/v1"
+
+        with patch.object(requests.Session, 'put', return_value=req_response) as mock_req_put, \
+                patch('spccore.baseclient._handle_response', return_value=body) as mock_handle, \
+                patch('spccore.baseclient._generate_signed_headers', return_value=headers) as mock_sign_headers, \
+                patch.object(req_response, "json", return_value=json):
+            assert client.put(path, body, request_parameters=params, endpoint=endpoint) == body
+            mock_req_put.assert_called_once_with(endpoint + path,
+                                                  data=json.dumps(body),
+                                                  headers=headers,
+                                                  params=params)
+            mock_handle.assert_called_once_with(req_response)
+            mock_sign_headers.assert_called_once_with(endpoint + path,
+                                                      username=username,
+                                                      api_key=base64.b64decode(api_key),
+                                                      headers=None)
+
+    # delete
+
+    def test_delete_use_default_endpoint(self, client_setup, test_data):
+        username, api_key, client = client_setup
+        req_response, path, headers, stub_response, params = test_data
+
+        with patch.object(requests.Session, 'delete', return_value=req_response) as mock_req_delete, \
+                patch('spccore.baseclient._handle_response', return_value=stub_response) as mock_handle, \
+                patch('spccore.baseclient._generate_signed_headers', return_value=headers) as mock_sign_headers, \
+                patch.object(req_response, "json", return_value=json):
+            assert client.delete(path, request_parameters= params) == stub_response
+            mock_req_delete.assert_called_once_with(SYNAPSE_DEFAULT_REPO_ENDPOINT+path, headers=headers, params=params)
+            mock_handle.assert_called_once_with(req_response)
+            mock_sign_headers.assert_called_once_with(SYNAPSE_DEFAULT_REPO_ENDPOINT+path,
+                                                      username=username,
+                                                      api_key=base64.b64decode(api_key),
+                                                      headers=None)
+
+    def test_delete_custom_endpoint(self, client_setup, test_data):
+        username, api_key, client = client_setup
+        req_response, path, headers, stub_response, params = test_data
+        endpoint = "https://repo-dev.dev.sagebase.org/repo/v1"
+
+        with patch.object(requests.Session, 'delete', return_value=req_response) as mock_req_delete, \
+                patch('spccore.baseclient._handle_response', return_value=stub_response) as mock_handle, \
+                patch('spccore.baseclient._generate_signed_headers', return_value=headers) as mock_sign_headers, \
+                patch.object(req_response, "json", return_value=json):
+            assert client.delete(path, request_parameters= params, endpoint=endpoint) == stub_response
+            mock_req_delete.assert_called_once_with(endpoint+path, headers=headers, params=params)
+            mock_handle.assert_called_once_with(req_response)
+            mock_sign_headers.assert_called_once_with(endpoint+path,
+                                                      username=username,
+                                                      api_key=base64.b64decode(api_key),
+                                                      headers=None)
