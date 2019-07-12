035af1aa4d4983fd255360d2c957b7599395a97e
KimYen Ladia <kimyen0816@gmail.com>
2019-07-12 15:00:22 -0700

    update docs

--- spccore/internal/timeutils.py
+++ spccore/internal/timeutils.py
@@ -1,19 +1,26 @@
 import datetime
 import platform
 
+"""
+Epoch time is in milisecond precision. 
+In Python time.time() and datetime.datetime.utcfromtimestamp() operates on float with milisecond precision.
+For example 123.456 represents 123456 miliseconds.
+The methods above takes and returns float in milisecond precision even though the unit at the decimal point is second.
+"""
+
 UNIX_EPOCH = datetime.datetime(1970, 1, 1, 0, 0)
 
 
 def from_epoch_time_to_iso(epoch_time: float) -> str:
     """
-    Convert seconds since unix epoch to a string in ISO format
+    Convert epoch time in milisecond precision since midnight Jan 1, 1970 to a string in ISO format.
     """
     return None if epoch_time is None else from_datetime_to_iso(from_epoch_time_to_datetime(epoch_time))
 
 
 def from_datetime_to_iso(dt: datetime.datetime) -> str:
     """
-    Round microseconds to milliseconds and add back the "Z" (timezone) at the end
+    Round microseconds to milliseconds and add back the "Z" (timezone) at the end.
 
     :param dt: the datetime object that represents a time
     :return: a string representation of the datetime of object
@@ -27,24 +34,24 @@ def from_datetime_to_iso(dt: datetime.datetime) -> str:
     return fmt.format(time=dt, millisecond=int(round(dt.microsecond / 1000.0)), tz="Z", sep="T")
 
 
-def from_epoch_time_to_datetime(time_ms: float) -> datetime.datetime:
+def from_epoch_time_to_datetime(epoch_time: float) -> datetime.datetime:
     """
     Returns a datetime object representation of a given time in milliseconds since midnight Jan 1, 1970.
 
-    :param time_ms: time in milliseconds since midnight Jan 1, 1970
+    :param epoch_time: time in millisecond precision since midnight Jan 1, 1970
     """
 
     # utcfromtimestamp() fails for negative values (dates before 1970-1-1) on Windows
     # so, here's a hack that enables ancient events, such as Chris's birthday, to be
     # converted from milliseconds since the UNIX epoch to higher level Datetime objects. Ha!
-    if platform.system() == 'Windows' and time_ms < 0:
-        mirror_date = datetime.datetime.utcfromtimestamp(abs(time_ms))
+    if platform.system() == 'Windows' and epoch_time < 0:
+        mirror_date = datetime.datetime.utcfromtimestamp(abs(epoch_time))
         return UNIX_EPOCH - (mirror_date - UNIX_EPOCH)
-    return datetime.datetime.utcfromtimestamp(time_ms)
+    return datetime.datetime.utcfromtimestamp(epoch_time)
 
 
 def from_datetime_to_epoch_time(dt: datetime) -> float:
     """
-    Convert either datetime.datetime objects to UNIX time.
+    Convert either datetime.datetime objects to epoch time in milisecond precision.
     """
     return (dt - UNIX_EPOCH).total_seconds()
