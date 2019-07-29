7e0e65d3d36451dfc4c489e5eb868d5abe3f2ea3
KimYen Ladia <kimyen0816@gmail.com>
2019-07-29 11:15:13 -0700

    refactor

--- tests/unit/mock_utils.py
+++
@@ -1,5 +0,0 @@
-# For Python 3.5, Mock's call list is not ordered
-def assert_call_list_equals(expected: list, actual: list):
-    assert len(expected) == len(actual)
-    for c in expected:
-        assert c in actual
